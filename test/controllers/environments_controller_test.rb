64ed6213b27477a8e3fcd88f5c13d01c79b1d8fb
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-02-19 11:16:06 -0300

    tests: fix controllers

--- test/controllers/environments_controller_test.rb
+++ test/controllers/environments_controller_test.rb
@@ -6,31 +6,31 @@ class EnvironmentsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get environments_url, as: :json
+    get environments_url
     assert_response :success
   end
 
   test "should create environment" do
     assert_difference('Environment.count') do
-      post environments_url, params: { environment: { destination_crt: @environment.destination_crt, name: @environment.name } }, as: :json
+      post environments_url, params: { environment: { destination_crt: @environment.destination_crt, name: @environment.name } }
     end
 
     assert_response 201
   end
 
   test "should show environment" do
-    get environment_url(@environment), as: :json
+    get environment_url(@environment)
     assert_response :success
   end
 
   test "should update environment" do
-    patch environment_url(@environment), params: { environment: { destination_crt: @environment.destination_crt, name: @environment.name } }, as: :json
+    patch environment_url(@environment), params: { environment: { destination_crt: @environment.destination_crt, name: @environment.name } }
     assert_response 200
   end
 
   test "should destroy environment" do
     assert_difference('Environment.count', -1) do
-      delete environment_url(@environment), as: :json
+      delete environment_url(@environment)
     end
 
     assert_response 204
